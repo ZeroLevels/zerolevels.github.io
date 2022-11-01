faccf7d5a77d203d3b07f823a5096e6acafdcb5d
ZeroLevels <ZeroLevels@yahoo.com>
2022-11-01 19:04:36 -0400

    Added a list of Vendors.
    Only shows base game stuff at the moment.

---
+++ ieVendors/info/functions/infoGrabber.js
@@ -0,0 +1,19 @@
+var itemType = {
+	AMMO: " (Ammo)",
+	ALCH: " (Aid)",
+	ARMO: " (Apparel)",
+	BOOK: " (Misc)",
+	MISC: " (Misc)",
+	WEAP: " (Weapon)",
+	LVLI: ""
+}
+
+function getList (e) {
+	var list = e ? e.split(',').map(items=>items.split('|')) : ""
+	return e ? list.map(item=>"<span itemsearch='"+uniquesSimplified[item[0]].toLowerCase()+"'class='item'>" + item[2] + "x " + uniquesSimplified[item[0]] + itemType[item[1]] + "</span>").join("<br>") : "Empty"
+}
+
+function getItemsOnly (e) {
+	var list = e ? e.split(',').map(items=>items.split('|')) : ""
+	return e ? list.map(item=>uniquesSimplified[item[0]]) : ["Empty"]
+}
